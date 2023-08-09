939d3cbd1531eaf1d73f34142cf7c0ada30b721a
Bestian Tang 大巴 <bestian@gmail.com>
2023-08-09 15:09:40 +0800

    add countDiff

---
+++ src/countDiff.js
@@ -0,0 +1,40 @@
+const inputRealUser1 = {
+    tags: ['happy', 'good', 'fine']
+}
+const inputRealUser2 = {
+    tags: ['foo', 'good', 'bar']
+}
+
+
+function conuntDimensions(inputRealUser1, inputRealUser2) {
+
+    let t1 = inputRealUser1.tags
+    let t2 = inputRealUser2.tags
+
+    const alltags = Set(t1.concact(t2))
+
+    return alltags
+}
+
+
+
+
+const inputUser1 = [0, 1, 0, 1, 1];
+const inputUser2 = [0, 1, 1, 0, 0];
+
+// add Error case ....
+function manhattanDistance(arr1, arr2) {
+  let distance = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    distance += Math.abs(arr1[i] - arr2[i]);
+  }
+  return distance;
+}
+
+const distance = manhattanDistance(inputUser1, inputUser2);
+console.log("曼哈頓距離是:", distance);
+
+
+module.exports = { 
+    manhattanDistance
+}
--- src/parseTags.js
+++ src/parseTags.js
@@ -1,3 +1,25 @@
+const inputRealUser1 = {
+    tags: ['happy', 'good', 'fine']
+}
+const inputRealUser2 = {
+    tags: ['foo', 'good', 'bar']
+}
+
+// 算出兩個用戶的標籤維度
+function countDimensions(inputRealUser1, inputRealUser2) {
+    let t1 = inputRealUser1.tags;
+    let t2 = inputRealUser2.tags;
+    const alltags = new Set(t1.concat(t2)); // 更正拼寫並使用 new Set()
+    return Array.from(alltags); // 轉換集合為數組
+}
+
+const uniqueTags = countDimensions(inputRealUser1, inputRealUser2);
+console.log("合併後的標籤（無重複）:", uniqueTags);
+// 這個函數將標籤字串轉換為標籤數組
+
+
+
+
 
 
 // add Error case ....
